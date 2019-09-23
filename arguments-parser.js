71b96b0c4e6ffb6d17aee51efed211a5bdbc882f
Alexis Aguirre <alex08lta@gmail.com>
2019-09-23 17:38:23 -0300

    v1.0.2-rev1

--- arguments-parser.js
+++ arguments-parser.js
@@ -5,7 +5,7 @@ function readMapping(){
         const data = readFileSync(__dirname + '/../../config/arguments-parser.json', { flag: 'r', encoding: 'utf8' })
         return data ? JSON.parse(data) : null
     }catch(err){
-        console.log('arguments-parser not configured. Please create arguments-parser.json inside config in project root folder.')
+        console.log('arguments-parser not configured. Please create arguments-parser.json inside config folder in project root folder.')
         return null
     }
 }
@@ -16,7 +16,9 @@ const processArgs = () => {
     for(let i = 0 ; i < arguments.length ; i++){
         let compact = arguments[i].indexOf('=') > 0
         if( !compact ){
-            if(!arguments[i + 1].startsWith('-')){
+            if(arguments[i + 1] === undefined){
+                arguments[i] = `${arguments[i]}=true`
+            }else if(!arguments[i + 1].startsWith('-')){
                 arguments[i] = `${arguments[i]}=${arguments[i+1]}`
                 arguments.splice(i + 1, 1)
             }else{
@@ -31,20 +33,43 @@ const processArgs = () => {
     return processed
 }
 
+function parseFinal(preProcessedJson){
+    for(let key in preProcessedJson){
+        if(!isNaN(parseInt(preProcessedJson[key], 10))){
+            preProcessedJson[key] = parseInt(preProcessedJson[key])
+            continue
+        }
+        switch(preProcessedJson[key]){
+            case 'true':
+                preProcessedJson[key] = true
+                break
+            case 'false':
+                preProcessedJson[key] = false 
+        }
+    }
+    return preProcessedJson
+}
+
 function argumentsParser() {
     const args = processArgs()
+    console.log(args)
     const mapping = readMapping()
     let result = {}
     if(mapping){
         for(let map of mapping){
             let key
             if(typeof(map) === 'object'){
-                // TODO: hacer funcionar el multiple mapeo de flags
                 const baseKey = map[0]
                 for(let key of map){
                     let keyValue = args.filter( arg => arg[0] === key)[0]
-                    if(keyValue && keyValue[0].length > 0 && !result[baseKey])
-                        result[key] = keyValue[1]
+                    if(keyValue && keyValue[0].length > 0){
+                        result[baseKey] = keyValue[1]
+                    }
+                }
+                // establecer el base
+                let keyValue = args.filter( arg => arg[0] === baseKey)[0]
+                if(keyValue && keyValue[0].length > 0){
+                    result[baseKey] = keyValue[1]
                 }
             }else if(typeof(map) === 'string'){
                 key = map
@@ -54,7 +79,7 @@ function argumentsParser() {
             }
         }
     }
-    return result
+    return parseFinal(result)
 }
 
 module.exports = argumentsParser
--- readme.md
+++ readme.md
@@ -1,18 +1,51 @@
 # arguments-parser
+
 A simple and configurable node arguments parser
 
 ## Configuration
+
 1. Create a folder named _config_.
 2. Create a file named _arguments-parser.json_ inside _config_ folder.
 3. Create an array of available arguments strings inside _arguments-parser.json_ file.
 
 ## Example: _arguments-parser.json_
+
 ```js
-[
-    "--styles",
-    "--routing",
-]
+["--styles", "--routing", ["--port", "-p"]];
 ```
 
-# RoadMap
-- [ ] - Multiple flags to same config item
+Note that the third config element is an array of strings, it can be used to assign two or more flags to same configuration.
+This configuration for *arguments-parser* will produce this json when we call node with:
+
+```js
+node index.js --skipgit --port 4200 -p 3200 --styles=scss
+```
+
+### Result object
+```json
+    {
+        "--skipgit": true,
+        "--styles": "scss",
+        "--port": 4200
+    }
+```
+
+```js
+node index.js --skipgit --p 3200 --port 4200 --styles=scss
+```
+
+### Result object
+```json
+    {
+        "--skipgit": true,
+        "--styles": "scss",
+        "--port": 4200
+    }
+```
+
+>   As you can see, the first element of the array (for multiple flags to same configuration) is the key of the result object, and has more height than other flags (for same configuration, in this case "-p" vs "--port" is sended in de node command, the result will be builded with "--port" value)
+
+## Other considerations
+
+- In the previous examples the --style flag value has an equal before the value itself, you can define your values with '=' or with a whitespace.
+- If no value has passed into a parameter, by default equals true
