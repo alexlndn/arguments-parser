c45e7e770cebdc51bc1c8a47d75a1f7a0e4e782c
kutiny <alex08lta@gmail.com>
2023-08-15 11:51:27 -0300

    feat: types

--- dist/cjs/main.d.ts
+++ dist/cjs/main.d.ts
@@ -1,8 +1,8 @@
 export declare type argumentsParserParams = {
-    explicit: true; // use configuration file: default false
-    keepAsArray: true; // true: keep all values as arrays or false: if only one value to the item, the item config will be this single value (not an array) : default false
-    onlyParamsArray: true; // returns an array of received params : default false
-    offset: 2; // offset for removing default params of node : default 2
+    explicit: Boolean; // use configuration file: default false
+    keepAsArray: Boolean; // true: keep all values as arrays or false: if only one value to the item, the item config will be this single value (not an array) : default false
+    onlyParamsArray: Boolean; // returns an array of received params : default false
+    offset: number; // offset for removing default params of node : default 2
 };
 
 export declare function argumentsParser(params: argumentsParserParams): void;
